734ede4b56b866a3cb994f7f2af95984a42c3d28
laurenreid <lauren.reid@medchemica.com>
2026-05-21 13:59:03 +0100

    fixed help message

--- syndirella/cli.py
+++ syndirella/cli.py
@@ -119,7 +119,7 @@ def config_parser(syndirella_base_path: str):
     pipeline_parser.add_argument('--no_assert_scaffold_intra_geom_flatness', action='store_true',
                         help="Don't check scaffold for intra geometry or flatness.")
     pipeline_parser.add_argument('--substructure_check_products', action='store_true',
-                                 help="Only keep products that are substructures of their scaffolds.")
+                                 help="Only keep products that are superstructures of their scaffolds.")
     
     add_reaction_parser = subparsers.add_parser('add-reaction', 
                                                help='Add a new reaction to the library',
